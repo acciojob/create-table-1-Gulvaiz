5256cb4c9ede5a9cee82a04e034aff1c2e1b5516
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-03 16:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let table = document.querySelector("table")
 function insert_Row() {
     //Write your code here
-  
-  
+  let newRow = document.createElement("tr")
+	newRow.innerHTML = ` <td>New Cell1</td> <td>New Cell2</td>`
+	table.prepend(newRow)
+   
 }
