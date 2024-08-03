ad6dc595220c78773b99ac3dfa2da764ca1891ad
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-03 17:03:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let table = document.querySelector("table")
 function insert_Row() {
     //Write your code here
-  let newRow = document.createElement("tr")
-	newRow.innerHTML = ` <td>New Cell1</td> <td>New Cell2</td>`
-	table.prepend(newRow)
-   
+let table = document.querySelector("table")
+
+	let newRow = table.insertRow(0)
+	let newCell1 = newRow.insertCell(0)
+     newCell1.innerText = "New Cell1"
+	let newCell2 = newRow.insertCell(1)
+    newCell2.innerText = "New Cell2"
 }
